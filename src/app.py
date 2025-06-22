5eefa3f29245d835b66590a32c733c613ef1ae43
Jacob King <jacobdking1999@icloud.com>
2025-06-22 15:20:04 -0400

    update .gitignore and requirements; load environment variables from .env file in app.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,16 @@
+import os
+from dotenv import load_dotenv
 from dash import Dash, dcc
 from components.layout import make_layout
 import dash_bootstrap_components as dbc
 from cache import cache  # Import the centralized cache
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get environment variables
+host = os.getenv("HOST", "0.0.0.0")  # Default to 0.0.0.0 if HOST is not set
+port = int(os.getenv("PORT", 8080))  # Default to 8080 if PORT is not set
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = "Quarterback Qualities"
@@ -13,4 +22,4 @@ cache.init_app(app.server)
 app.layout = make_layout()  # Remove global loading spinner for deployment
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)  # Use host and port suitable for Render
+    app.run(host=host, port=port)  # Use host and port from environment variables
